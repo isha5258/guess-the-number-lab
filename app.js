d235cb24351d0aabaf6e170a7252c2827bda4cbb
IsaacH <Isaac.hathaway28@gmail.com>
2022-01-30 08:31:22 -0700

    add get guess add

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  },
+  getGuess: function(){
+     return `Enter a guess between ${smallestNum} and ${biggestNum}`
   }
 }
