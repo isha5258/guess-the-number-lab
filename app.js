31eb383a1a0e87bcb75f6f65b19e0a64b65b73a3
IsaacH <Isaac.hathaway28@gmail.com>
2022-01-30 10:48:20 -0700

    complete guess the number game

--- app.js
+++ app.js
@@ -2,15 +2,42 @@
 
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
-  smallestNum: 1,
+  biggestNum: 10,
+  smallestNum: 0,
   secretNum: null,
   prevGuesses: [],
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  },
+      do {
+        this.prevGuesses.push(this.getGuess());
+        this.render();
+      }while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum);
+      
+    },
+  
   getGuess: function(){
-     return `Enter a guess between ${smallestNum} and ${biggestNum}`
-  }
+    let guess = null
+    
+    do {
+      guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+
+    } while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum)
+    return guess
+    },
+
+  render: function(){
+    console.log(this.prevGuesses)
+    if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum){
+    console.log(`Congrat! You guessed the number in ${this.prevGuesses[this.prevGuesses.length - 1]} tries!`)
+  } else if (this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum){
+    
+    console.log(`Your guess is too high. Previous guesses: ${this.prevGuesses.join(', ')}`)
+  } else {
+    console.log(`Your guess is too low. Previous guesses: ${this.prevGuesses.join(', ')}`)
 }
+}
+}
+game.play()
+
